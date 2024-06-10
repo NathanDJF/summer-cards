9321dddbe6f62696d9de12e3b813b6040c544094
NathanDJF <nathandjfcodes@gmail.com>
2024-06-10 18:43:53 +0700

    summer2.

--- main.py
+++ main.py
@@ -2,4 +2,17 @@ import pygame
 import sys
 import os
 
-#test
+pygame.init()
+
+screen = pygame.display.set_mode((900, 500))
+pygame.display.set_caption('Summer Cards')
+#pygame.display.set_icon('Assets/icon.png')
+
+while True:
+    screen.fill((255, 127, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
